0cfd8d0f4038c9540624f68ea11094c4a49db714
vath.tr-dev <vthien562004@gmail.com>
2024-11-08 20:15:37 +0700

    Update categoryController.js

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -1,5 +1,5 @@
 // controllers/categoryController.js
-const Category = require('../models/Category');
+const Category = require('../Models/Category');
 
 const categoryController = {
     // Lấy tất cả các danh mục
